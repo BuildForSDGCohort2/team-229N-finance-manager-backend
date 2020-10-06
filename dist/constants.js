6d3120e420acc5c8dccaf9ec8fdd183873d7f423
Herbert <herberthtk100@gmail.com>
2020-10-06 13:32:49 +0300

    Prepared code for deployment

--- dist/constants.js
+++ dist/constants.js
@@ -1,9 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SERVER_URL = exports.FORGET_PASSWORD_PREFIX = exports.COOKIE_NAME = exports.__prod__ = void 0;
+const is_ci_1 = __importDefault(require("is-ci"));
 exports.__prod__ = process.env.NODE_ENV === 'production';
 exports.COOKIE_NAME = 'qid';
 exports.FORGET_PASSWORD_PREFIX = 'forget-password:';
-exports.SERVER_URL = !exports.__prod__
-    ? 'http://localhost:8000'
-    : 'https://cohot2.herokuapp.com';
+exports.SERVER_URL = exports.__prod__ || is_ci_1.default ? 'https://cohot2.herokuapp.com' : 'http://localhost:8000';
--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
+import isCI from 'is-ci';
 export const __prod__ = process.env.NODE_ENV === 'production';
 export const COOKIE_NAME = 'qid';
 export const FORGET_PASSWORD_PREFIX = 'forget-password:';
-export const SERVER_URL = !__prod__
-  ? 'http://localhost:8000'
-  : 'https://cohot2.herokuapp.com';
+export const SERVER_URL =
+  __prod__ || isCI ? 'https://cohot2.herokuapp.com' : 'http://localhost:8000';
